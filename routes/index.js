9d861df07c4907ff0ed5d15db2f47c1772643c92
Che-An <an5566x@gmail.com>
2019-01-10 19:01:31 +0800

    1/10

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const Web3 = require('web3');
+
+const web3 = new Web3('http://localhost:8545');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//unlock account
+router.post('/unlock', function (req, res, next) {
+  web3.eth.personal.unlockAccount(req.body.account, req.body.password, 60)
+    .then(function (result) {
+      res.send('true')
+    })
+    .catch(function (err) {
+      res.send('false')
+    })
+});
+
+//deploy Mytoken contract
+router.post('/deploy', function (req, res, next) {
+  let bank = new web3.eth.Contract(contract.abi);
+  bank.deploy({
+    data: contract.bytecode
+  })
+    .send({
+      from: req.body.account,
+      gas: 3400000
+    })
+    .on('receipt', function (receipt) {
+      res.send(receipt);
+    })
+    .on('error', function (error) {
+      res.send(error.toString());
+    })
 });
 
 module.exports = router;
